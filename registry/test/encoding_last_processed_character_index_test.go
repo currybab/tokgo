513314c388cab156b8a69d0b6442be020a19c018
Jun Park <pj2417@gmail.com>
2025-06-06 23:21:14 +0900

    add encoding last processed character index test

---
+++ registry/test/encoding_last_processed_character_index_test.go
@@ -0,0 +1,31 @@
+package registry_test
+
+import (
+	"testing"
+
+	"github.com/currybab/tokgo"
+	"github.com/currybab/tokgo/registry"
+	"github.com/stretchr/testify/assert"
+)
+
+var ENCODING, _ = registry.NewDefaultEncodingRegistry().GetEncodingByType(tokgo.CL100K_BASE)
+
+func TestNullInput(t *testing.T) {
+	encodingResult := ENCODING.Encode("", 10)
+	assert.Equal(t, encodingResult.GetLastProcessedCharacterIndex(), -1)
+}
+
+func TestEmptyInput(t *testing.T) {
+	encodingResult := ENCODING.Encode("", 10)
+	assert.Equal(t, encodingResult.GetLastProcessedCharacterIndex(), -1)
+}
+
+func TestShortInput(t *testing.T) {
+	encodingResult := ENCODING.Encode("Hello World!", 10)
+	assert.Equal(t, encodingResult.GetLastProcessedCharacterIndex(), 11)
+}
+
+func TestLongInput(t *testing.T) {
+	encodingResult := ENCODING.Encode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce condimentum enim ac tellus malesuada, a consectetur nibh efficitur. 🚀🚀🚀", 10)
+	assert.Equal(t, encodingResult.GetLastProcessedCharacterIndex(), 55)
+}
